1161c94a5c27a9e376a9f38c731fe3e4ecbc858c
Steve Ruff <gsruff@gatech.edu>
2021-01-31 19:36:10 -0500

    added output path option and updated README

--- README.md
+++ README.md
@@ -5,17 +5,19 @@ A simple script to read hex color values from a text file and output solid 'colo
 - [Pillow](https://github.com/python-pillow/Pillow "Pillow")
 ## Usage
 ```
-    usage: colorchip.py [-h] -i FILE [-d DIMENSIONS]
-    
+    usage: colorchip.py [-h] -i FILE [-d DIMENSIONS] [-o OUTPUT]
+
     required arguments:
       -i FILE, --input FILE
                             input file
-    
+
     optional arguments:
       -d DIMENSIONS, --dimensions DIMENSIONS
                             dimensions of generated image in x,y format. default is 100,100.
+      -o OUTPUT, --output OUTPUT
+                            output path
 ```
 ## Input File
 See example 'colors.txt'. Beginning lines with # is also allowed.
 ## Output
-Output image files are in PNG format and are saved to the same location as colorchip.py.
+Output image files are in PNG format and are saved to the same location as colorchip.py, unless the --output argument has been supplied.
--- colorchip.py
+++ colorchip.py
@@ -1,5 +1,6 @@
 import argparse
 import re
+import os
 from PIL import Image
 
 def dims(s):
@@ -9,6 +10,15 @@ def dims(s):
     except:
         raise argparse.ArgumentTypeError('Dimensions must be x,y')
 
+def writable_dir(s):
+    if not os.path.isdir(s):
+        raise argparse.ArgumentTypeError('Supplied path {} is not a valid path.'.format(s))
+    if os.access(s, os.W_OK):
+        return s
+    else:
+        raise argparse.ArgumentTypeError('Supplied path {} is not writable by the current user.'.format(s))
+
+# setup required and optional command line arguments
 parser = argparse.ArgumentParser()
 parser._action_groups.pop()
 required = parser.add_argument_group('required arguments')
@@ -18,22 +28,25 @@ required.add_argument('-i', '--input', dest='file', help='input file',
 optional.add_argument('-d', '--dimensions',
                     help='dimensions of generated image in x,y format. default is 100,100.',
                     type=dims, default=(100,100))
+optional.add_argument('-o', '--output', help='output path', type=writable_dir, default=os.getcwd())
 args = parser.parse_args()
 
+# parse the input file and output images
 with args.file as colors:
     line_num = 1
     for line in colors:
         color = ''
+        # strip leading # and trailing \n characters
         if line.startswith('#'):
             color = line[1:-1].strip()
         else:
             color = line.strip()
+        # regex match hex strings
         hex = re.search(r'^(?:[0-9a-fA-F]{3}){1,2}$', color)
         if hex:
             im = Image.new('RGB',args.dimensions,'#' + color)
-            # TODO: Add argument to allow specifying output path
             # TODO: Add argument to allow specifying image format
-            im.save(color + '.png')
+            im.save(args.output + '/' + color + '.png')
         else:
             print('Hex color code \'{}\' on line {} is invalid.'.format(color,line_num))
         line_num += 1
